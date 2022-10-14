de26c5187a38a42123a44fdb9a56ba1753847c32
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-10-14 21:11:46 +0530

    Add project files.

---
+++ EAD Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EAD_Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
